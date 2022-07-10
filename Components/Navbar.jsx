485f39caa196e2eaa19d4adf1986584044576038
Ankit-Mohanta <mohantaankit2002@gmail.com>
2022-07-10 14:51:57 +0530

    Initialized first next js's git reposetory

---
+++ Components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.mainnav}>
+        <ul>
+          <Link href='/'>
+          <li>
+            Home
+          </li>
+          </Link>
+          <Link href='/About'>
+          <li>
+            About
+          </li>
+          </Link>
+          <Link href='/Blog'>
+          <li>
+            Blog
+          </li>
+          </Link>
+          <Link href='/Contact'>
+          <li>
+            Contact
+          </li>
+          </Link>
+        </ul>
+      </nav>
+  )
+}
+
+export default Navbar
